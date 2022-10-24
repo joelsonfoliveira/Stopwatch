70db552a37030979cd1829a78388135a1779a74b
Joelson Freitas de Oliveira <joelsonfoliveira@gmail.com>
2022-10-24 09:03:54 -0300

    Estrutura do cronômetro

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace Stopwatch
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+
+    }
+
+    static void Start()
+    {
+      int time = 10;
+      int currentTime = 0;
+      while (currentTime != time)
+      {
+        currentTime++;
+      }
+    }
+  }
+}
