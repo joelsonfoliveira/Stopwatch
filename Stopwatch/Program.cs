2eae9e19a8df62b16727051560a0d254c29b6e3e
Joelson Freitas de Oliveira <joelsonfoliveira@gmail.com>
2022-10-26 23:34:24 -0300

    Criando o menu

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -5,12 +5,21 @@ namespace Stopwatch
   {
     public static void Main(string[] args)
     {
-      Start();
+      // Start(6);
+      Menu();
     }
 
-    static void Start()
+    static void Menu()
     {
-      int time = 10;
+      Console.Clear();
+      System.Console.WriteLine("S - Segundo");
+      System.Console.WriteLine("M - Minuto");
+      System.Console.WriteLine("0 -  Sair");
+      System.Console.WriteLine("Quanto tempo deseja contar?");
+    }
+
+    static void Start(int time)
+    {
       int currentTime = 0;
       while (currentTime != time)
       {
@@ -19,6 +28,10 @@ namespace Stopwatch
         System.Console.WriteLine(currentTime);
         Thread.Sleep(1000);
       }
+
+      Console.Clear();
+      System.Console.WriteLine("Stopwatch finalizado.. retornando para o menu!");
+      Thread.Sleep(2500);
     }
   }
 }
