6db475a062bac6f3ae172b473bdb6f7ed7a36ca0
JOELSON FREITAS DE OLIVEIRA <joelsonfoliveira@gmail.com>
2022-10-28 14:26:53 -0300

    Finalizando a aplicação

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -24,13 +24,26 @@ namespace Stopwatch
       if (multiplier == 'm')
         multiplier = 60;
 
-      if (type == '0')
+      if (time == 0)
         System.Environment.Exit(0);
 
-      Start(time * multiplier);
+      PreStart(time * multiplier);
 
       System.Console.WriteLine(type);
       System.Console.WriteLine(time);
+    }
+
+    static void PreStart(int time)
+    {
+      Console.Clear();
+      System.Console.WriteLine("Ready...");
+      Thread.Sleep(1000);
+      System.Console.WriteLine("Set...");
+      Thread.Sleep(1000);
+      System.Console.WriteLine("Go...");
+      Thread.Sleep(2500);
+
+      Start(time);
     }
 
     static void Start(int time)
